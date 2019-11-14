e197c14b4fa283f27dfebae040e125858d4afabe
hustme <hustme@126.com>
2019-11-14 22:27:59 +0800

    添加悬浮球

--- app/src/main/java/priv/jiangtao/countdown/MainActivity.java
+++ app/src/main/java/priv/jiangtao/countdown/MainActivity.java
@@ -1,18 +1,14 @@
 package priv.jiangtao.countdown;
 
 import android.app.Activity;
-import android.app.Application;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.huxq17.floatball.libarary.FloatBallManager;
 import com.huxq17.floatball.libarary.floatball.FloatBallCfg;
-import com.huxq17.floatball.libarary.menu.FloatMenuCfg;
-import com.huxq17.floatball.libarary.menu.MenuItem;
 import com.huxq17.floatball.libarary.utils.BackGroudSeletor;
 import com.huxq17.floatball.libarary.utils.DensityUtil;
 
@@ -30,17 +26,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        boolean showMenu = false;//换成false试试
-        init(showMenu);
+        init();
         //5 如果没有添加菜单，可以设置悬浮球点击事件
-        if (mFloatballManager.getMenuItemSize() == 0) {
-            mFloatballManager.setOnFloatBallClickListener(new FloatBallManager.OnFloatBallClickListener() {
-                @Override
-                public void onFloatBallClick() {
-                    toast("点击了悬浮球");
-                }
-            });
-        }
+        mFloatballManager.setOnFloatBallClickListener(new FloatBallManager.OnFloatBallClickListener() {
+            @Override
+            public void onFloatBallClick() {
+                toast("点击了悬浮球");
+            }
+        });
     }
 
     @Override
@@ -57,31 +50,12 @@ public class MainActivity extends Activity {
     }
 
 
-    private void init(boolean showMenu) {
+    private void init() {
         //1 初始化悬浮球配置，定义好悬浮球大小和icon的drawable
         int ballSize = DensityUtil.dip2px(this, 45);
         Drawable ballIcon = BackGroudSeletor.getdrawble("ic_floatball", this);
-        //可以尝试使用以下几种不同的config。
-//        FloatBallCfg ballCfg = new FloatBallCfg(ballSize, ballIcon);
-//        FloatBallCfg ballCfg = new FloatBallCfg(ballSize, ballIcon, FloatBallCfg.Gravity.LEFT_CENTER,false);
-//        FloatBallCfg ballCfg = new FloatBallCfg(ballSize, ballIcon, FloatBallCfg.Gravity.LEFT_BOTTOM, -100);
-//        FloatBallCfg ballCfg = new FloatBallCfg(ballSize, ballIcon, FloatBallCfg.Gravity.RIGHT_TOP, 100);
-        FloatBallCfg ballCfg = new FloatBallCfg(ballSize, ballIcon, FloatBallCfg.Gravity.RIGHT_CENTER);
-        //设置悬浮球不半隐藏
-        ballCfg.setHideHalfLater(false);
-        if (showMenu) {
-            //2 需要显示悬浮菜单
-            //2.1 初始化悬浮菜单配置，有菜单item的大小和菜单item的个数
-            int menuSize = DensityUtil.dip2px(this, 180);
-            int menuItemSize = DensityUtil.dip2px(this, 40);
-            FloatMenuCfg menuCfg = new FloatMenuCfg(menuSize, menuItemSize);
-            //3 生成floatballManager
-            mFloatballManager = new FloatBallManager(getApplicationContext(), ballCfg, menuCfg);
-            addFloatMenuItem();
-
-        } else {
-            mFloatballManager = new FloatBallManager(getApplicationContext(), ballCfg);
-        }
+        FloatBallCfg ballCfg = new FloatBallCfg(ballSize, ballIcon, FloatBallCfg.Gravity.RIGHT_CENTER, false);
+        mFloatballManager = new FloatBallManager(getApplicationContext(), ballCfg);
         setFloatPermission();
     }
 
@@ -110,42 +84,9 @@ public class MainActivity extends Activity {
     }
 
 
-
     private void toast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void addFloatMenuItem() {
-        MenuItem personItem = new MenuItem(BackGroudSeletor.getdrawble("ic_weixin", this)) {
-            @Override
-            public void action() {
-                toast("打开微信");
-                mFloatballManager.closeMenu();
-            }
-        };
-        MenuItem walletItem = new MenuItem(BackGroudSeletor.getdrawble("ic_weibo", this)) {
-            @Override
-            public void action() {
-                toast("打开微博");
-            }
-        };
-        MenuItem settingItem = new MenuItem(BackGroudSeletor.getdrawble("ic_email", this)) {
-            @Override
-            public void action() {
-                toast("打开邮箱");
-                mFloatballManager.closeMenu();
-            }
-        };
-        mFloatballManager.addMenuItem(personItem)
-                .addMenuItem(walletItem)
-                .addMenuItem(personItem)
-                .addMenuItem(walletItem)
-                .addMenuItem(settingItem)
-                .buildMenu();
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 }
